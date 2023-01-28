f9de7afec7ebf5d6b65e0c685452e5c2b092c1fb
Sahil1728 <77491529+Sahil1728@users.noreply.github.com>
2023-01-28 04:43:02 +0000

    Sorting in ascending order using insertionSort

---
+++ Chapter 02/insertionSort.cpp
@@ -0,0 +1,46 @@
+// Implementation of the insertion sort algorithm.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void insertionSortAscending(vector<int> &arr){
+    int i=1, n=arr.size();
+    // We consider that that array upto [:i) is sorted
+    for(i;i<n;i++){
+        int key=arr[i];
+        // inserting the key in the sorted subarray to its left
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            // shifting greater elements one step right
+            arr[j+1]=arr[j];
+            j--;
+        }
+        // inserting key at the its designated position in the sorted subarray
+        arr[j+1]=key;
+    }
+}
+
+// main function
+int main(){
+    // taking the array as input in form of vector
+    cout<<"Enter the size of the array:";
+    int n;cin>>n;
+    // declaration of vector
+    vector<int> arr;
+    cout<<"Enter the elements of the array:"<<endl;
+    for(int i=0;i<n;i++){
+        int x;cin>>x;
+        arr.push_back(x);
+    }
+    cout<<"Array before sorting:"<<endl;
+    for(auto x:arr){
+        cout<<x<<" ";
+    }
+    // calling the insertion sort function
+    insertionSortAscending(arr);
+    cout<<endl<<"Array after sorting in ascending order:"<<endl;
+    for(auto x:arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
